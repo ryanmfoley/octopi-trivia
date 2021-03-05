2996e2b2c3af8e97b497025f68bc93db11e81cf6
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-03-05 16:25:23 -0500

    push

--- controllers/trivia_controller.js
+++ controllers/trivia_controller.js
@@ -2,10 +2,10 @@ const express = require('express')
 const trivias = express.Router()
 const axios = require('axios')
 
-let mathQuestion = ""
+let mathObject = ""
 axios.get('https://opentdb.com/api.php?amount=15&category=19&difficulty=medium&type=multiple').then((response) => {
-    console.log(response.data["results"][0]["question"])
-    mathQuestion = response.data["results"][0]["question"]
+    // console.log(response.data["results"])
+    mathObject = response.data["results"]
 
 }).catch((error) => {
     console.log(error);
@@ -31,13 +31,14 @@ trivias.get('/', isAuthenticated, (req, res) => {
 })
 
 
-trivias.get('/math', isAuthenticated, (req, res) => {
+trivias.get('/math/:questionNum', isAuthenticated, (req, res) => {
+
     res.render('trivias/math.ejs',{
         currentUser: req.session.currentUser,
-        mathQuestion: mathQuestion
+        mathObject: mathObject[req.params.questionNum],
+        num: req.params.questionNum
     })
 })
-
 
 
 module.exports = trivias
