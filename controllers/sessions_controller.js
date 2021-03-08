be39622d27f11f48549c6040221c45ca4ff0724c
ryfoa6 <duckduckgoose384@gmail.com>
2021-03-07 20:59:36 -0800

    complete

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -4,31 +4,31 @@ const sessions = express.Router()
 const User = require('../models/users.js')
 
 sessions.get('/new', (req, res) => {
-  res.render('sessions/new.ejs', { currentUser: req.session.currentUser })
+	res.render('sessions/new.ejs', { currentUser: req.session.currentUser })
 })
 
 sessions.post('/', (req, res) => {
-  User.findOne({ username: req.body.username }, (err, foundUser) => {
-    if (err) {
-      console.log(err)
-      res.send('oops the db had a problem')
-    } else if (!foundUser) {
-      res.send('<a href="/">Sorry User not found</a>')
-    } else {
-      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-        req.session.currentUser = foundUser
-        res.redirect('/')
-      } else {
-        res.send('<a href="/"> Password does not match </a>')
-      }
-    }
-  })
+	User.findOne({ username: req.body.username }, (err, foundUser) => {
+		if (err) {
+			console.log(err)
+			res.send('oops the db had a problem')
+		} else if (!foundUser) {
+			res.send('<a href="/">Sorry User not found</a>')
+		} else {
+			if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+				req.session.currentUser = foundUser
+				res.redirect('/')
+			} else {
+				res.send('<a href="/"> Password does not match </a>')
+			}
+		}
+	})
 })
 
 sessions.delete('/', (req, res) => {
-  req.session.destroy(() => {
-    res.redirect('/')
-  })
+	req.session.destroy(() => {
+		res.redirect('/')
+	})
 })
 
 module.exports = sessions
